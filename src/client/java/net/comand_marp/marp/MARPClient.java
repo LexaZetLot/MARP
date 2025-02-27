a46bf0060d72905e38b64a69dd7a5f7b21f8f2d0
LexaZetLot <alexzpoka@gmail.com>
2025-02-27 19:09:30 +0300

    init commit

---
+++ src/client/java/net/comand_marp/marp/MARPClient.java
@@ -0,0 +1,10 @@
+package net.comand_marp.marp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MARPClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/net/comand_marp/marp/MARPDataGenerator.java
@@ -0,0 +1,11 @@
+package net.comand_marp.marp;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MARPDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
