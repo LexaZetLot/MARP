61c9a3d09a08124ee3ec32e5c5c27ba0dbec8f87
LexaZetLot <alexzpoka@gmail.com>
2025-02-28 13:02:04 +0300

    Deleted items
    removing everything to create a clean project

--- src/main/java/net/comand_marp/marp/MARP.java
+++ src/main/java/net/comand_marp/marp/MARP.java
@@ -1,6 +1,5 @@
 package net.comand_marp.marp;
 
-import net.comand_marp.marp.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -19,7 +18,5 @@ public class MARP implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		ModItems.registerModItems();
 	}
 }
